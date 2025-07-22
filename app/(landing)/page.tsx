d8212e5a619a17234ed81c55b02ac521f39c2666
ndiyo <ndiyoedward@gmail.com>
2025-07-22 09:03:46 +0100

    handing over to miracle to sort out all error before pushing to the server

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/pages/Home/Hero";
 import LogoTicker from "@/pages/Home/LogoTicker";
 import {AboutGame} from "@/constants/AboutGame";
-import Community from "@/pages/Home/Community";
+import { Community } from "@/pages/Home/Community";
 import Features  from "@/pages/Home/Features";
 import Sales  from "@/pages/Home/Sales";
 import Testimonials from "@/pages/Home/Testimonials";
--- pages/Home/Community.tsx
+++ pages/Home/Community.tsx
@@ -12,7 +12,7 @@ import DONOR from '@/public/donor.jpg'
 // temporary assets – replace with real paths later
 const AVATARS = ["/avatar-4.png", "/avatar-7.png", "/avatar-6.png"];
 
-const Community = () => {
+export  const Community = () => {
   const bullets = [
     {
       title: "For the Youth",
