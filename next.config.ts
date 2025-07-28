23c116e668adf7e0e0dd82fb34a925ed2f61311e
CheeseCode1001 <graycheese1001@gmail.com>
2025-07-28 14:00:11 +0100

    fixed errors

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true, // Optional: Adds trailing slashes to URLs for consistent routing
-  // ... other configurations
   images: {
     unoptimized: true,
   },
